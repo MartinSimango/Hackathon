0e167700d15019e8a8ad8a3411a83842be5aa5a8
MartinSimango <martin.simango@bsg.co.za>
2020-10-15 11:48:06 +0200

    Configure hibernate and add environment variables

--- hackathon-backend/.environment-variables.sh
+++ hackathon-backend/.environment-variables.sh
@@ -1,5 +1,5 @@
-export JDBC_URL=jdbc:mysql://localhost:3306/hackathon  #todo: Ian/Shannon change this to RDS address
-export JDBC_USER=root     #todo: Ian/Shannon change this to database user
-export JDBC_PASSWORD='password$123'  #todo: Ian/Shannon change this to database password
-export JDBC_DRIVER=com.mysql.cj.jdbc.Driver
+export HACKATHON_JDBC_URL='jdbc:mysql://localhost:10000/hackathon'  #todo: Ian/Shannon change this to RDS address
+export HACKATHON_JDBC_USER='root'     #todo: Ian/Shannon change this to database user
+export HACKATHON_JDBC_PASSWORD='hello'  #todo: Ian/Shannon change this to database password
+export HACKATHON_JDBC_DRIVER='com.mysql.cj.jdbc.Driver'
 export HIBERNATE_DIALECT='org.hibernate.dialect.MySQL8Dialect'
---
+++ hackathon-backend/src/main/java/hackathon/entity/Employee.java
@@ -0,0 +1,31 @@
+package hackathon.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = 0L;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "about_me", nullable = false)
+    private String aboutMe;
+
+    @NotNull
+    @Column(name = "tags", nullable = true)
+    private String tags;
+
+    //todo: Martin review lazy fetch type
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_allocation_id")
+    private ProjectAllocation projectAllocation;
+
+
+}
