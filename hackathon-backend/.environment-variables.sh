4f5269ecb03266dba15761daaae4545a1878ce03
MartinSimango <martin.simango@bsg.co.za>
2020-10-15 12:47:25 +0200

    Add credentials for RDS instance

--- hackathon-backend/.environment-variables.sh
+++ hackathon-backend/.environment-variables.sh
@@ -1,5 +1,5 @@
-export HACKATHON_JDBC_URL='jdbc:mysql://localhost:10000/hackathon'  #todo: Ian/Shannon change this to RDS address
-export HACKATHON_JDBC_USER='root'     #todo: Ian/Shannon change this to database user
-export HACKATHON_JDBC_PASSWORD='hello'  #todo: Ian/Shannon change this to database password
+export HACKATHON_JDBC_URL='jdbc:mysql://ghack2020db.cqdnabcbucma.eu-west-1.rds.amazonaws.com:3306/ghack2020db'
+export HACKATHON_JDBC_USER='admin'
+export HACKATHON_JDBC_PASSWORD='GDPHack2020'
 export HACKATHON_JDBC_DRIVER='com.mysql.cj.jdbc.Driver'
 export HIBERNATE_DIALECT='org.hibernate.dialect.MySQL8Dialect'
